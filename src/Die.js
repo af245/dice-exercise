d5d927ccb114c117da459d3c48ccbb0d157b00a1
Andres Franco <camilofrnc182@gmail.com>
2021-03-07 08:21:49 -0500

    first branch commit

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Die extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Die;
